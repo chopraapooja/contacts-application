2d3288ad83fde1137025df9c1854970b92a82164
Pooja Chopra <chopraapooja@gmail.com>
2017-09-18 11:30:18 +0530

    lein init

---
+++ test/contacts_application/core_test.clj
@@ -0,0 +1,7 @@
+(ns contacts-application.core-test
+  (:require [clojure.test :refer :all]
+            [contacts-application.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
