88d2a55998068bd240ba29a0f1eed8ef1a030124
Pooja Chopra <chopraapooja@gmail.com>
2017-09-18 16:03:27 +0530

    search function works prettymuch done with library of trie data-structure

--- src/contacts_application/trie.clj
+++ src/contacts_application/trie.clj
@@ -1,11 +1,5 @@
 (ns contacts-application.trie)
 
-;; (add-word tree word)
-;;           {}   "a"   =>  {:a {:is-word-completed true 
-;;                               :children {}
-;;                               :data ["a"]}
-;;                               :current_node }
-
 (def new-node {:childrens {}
                :is-word-completed false})
 
@@ -19,13 +13,51 @@
       (-> tree
           (assoc-in this-node-path new-node)))))
 
-(defn add-word [tree word]
-  (let [{:keys [prev-node-path] :as tree} 
-        (reduce add-word-reducer 
-                (assoc tree  :prev-node-path [])
-                word)]
-    (when (not-empty prev-node-path)
-      (-> tree
-          (assoc-in (concat prev-node-path [:is-word-completed]) true)
-          (update-in (concat prev-node-path [:data]) #(concat (or % [])
-                                                              [word]))))))
+(defn add-word 
+  ([tree word]
+   (add-word tree word word))
+  ([tree word data]
+   (let [{:keys [prev-node-path] :as tree} 
+         (reduce add-word-reducer 
+                 (assoc tree  :prev-node-path [])
+                 word)]
+     (when (not-empty prev-node-path)
+       (-> tree
+           (assoc-in (concat prev-node-path [:is-word-completed]) true)
+           (update-in (concat prev-node-path [:data]) #(concat (or % [])
+                                                               [data])))))))
+
+(defn find-complete-words 
+  ([tree]
+   (find-complete-words tree []))
+  ([tree result]
+   (if (and tree (empty? tree))
+     result
+     (let [result (if (:is-word-completed tree) 
+                    (concat result (:data tree))
+                    result)]
+       (reduce (fn [acc [k v]]
+                 (concat acc (find-complete-words v []))) 
+               result 
+               (:childrens tree))))))
+;; FIXME:
+;; Due to lack of time not able to write tests in the format of clojure.test
+;; (find-complete-words {} [])
+;; (find-complete-words (add-word {} "a") [])
+;; (find-complete-words (-> {} (add-word "a") (add-word "ant") (add-word "antique")) [])
+;; (find-complete-words (-> {} (add-word "chris") (add-word "chrisharris")) [])
+
+(defn traverse [tree path]
+    (loop [tree tree
+         path path]
+    (let [kee (-> path first str keyword)]
+     (cond
+      (empty? path) tree
+      (empty? tree) nil
+      (-> tree :childrens kee)
+      (recur (-> tree :childrens kee) (subs path 1))
+      :else nil))))
+
+(defn search-tree [tree search-text]
+  (let [tree-node (traverse tree search-text)]
+    (find-complete-words tree-node)))
--- test/contacts_application/trie_test.clj
+++ test/contacts_application/trie_test.clj
@@ -12,10 +12,60 @@
     (testing "Adds a multi-letter-word"
       (is (= (add-word {} "ab")
              {:prev-node-path '(:childrens :a :childrens :b), 
-              :childrens {:a {:childrens {:b {:childrens {}, :is-word-completed true, :data '("ab")}}, :is-word-completed false}}}))))
+              :childrens {:a {:childrens {:b {:childrens {}, :is-word-completed true, :data '("ab")}}, :is-word-completed false}}})))
+
+    (testing "Adds meta-data in the leaf node if given"
+        (is (= (add-word {} "a" {:a "I am a"})
+               {:prev-node-path '(:childrens :a), 
+                :childrens {:a {:childrens {}, :is-word-completed true, :data '({:a "I am a"})}}}))))
 
   (testing "with existing trie-tree"
     (testing "extending a tree "
       (let [tree (add-word {} "a")]
         (is (= (add-word tree "ab")
                {:prev-node-path '(:childrens :a :childrens :b), :childrens {:a {:childrens {:b {:childrens {}, :is-word-completed true, :data '("ab")}}, :is-word-completed true, :data '("a")}}}))))))
+
+
+(deftest search-tree
+  (testing "Cannot find 'abc' in a tree having ('def') "
+    (let [tree (add-word {} "def")]
+      (is (= (search-tree tree "abc")
+             []))))
+
+  (testing "Can find 'a' in a tree having ('a') "
+    (let [tree (add-word {} "a")]
+      (is (= (search-tree tree "a")
+             ["a"]))))
+
+  (testing "Can find 'a' in a tree having ('a') "
+    (let [tree (add-word {} "a")]
+      (is (= (search-tree tree "a")
+             ["a"]))))
+
+  (testing
+      (let [tree (-> (add-word {} "pin")
+                     (add-word "pig")
+                     (add-word "paste")
+                     (add-word "poo")
+                     (add-word "ant")
+                     (add-word "antique"))]
+
+          (testing "Can find ('pin','pig','paste','poo') when searched for 'p') "
+            (is (= (search-tree tree "p")
+                   ["pin" "pig" "paste" "poo"])))
+          
+          (testing "Can find ('pin','pig'') when searched for 'pi') "
+            (is (= (search-tree tree "pi")
+                   ["pin" "pig"])))
+
+          (testing "Can find ('ant', 'antique') when searched for 'ant') "
+            (is (= (search-tree tree "ant")
+                   ["ant" "antique"]))))))
+
+(deftest traverse-test
+  (testing "traverse test"
+      (is (= (traverse {} "a") nil))
+      (is (= (traverse {} "") {}))
+      (is (= (traverse (add-word {} "abc") "a")
+             {:childrens {:b {:childrens {:c {:childrens {}, :is-word-completed true, :data '("abc")}}, :is-word-completed false}}, :is-word-completed false}))
+))
