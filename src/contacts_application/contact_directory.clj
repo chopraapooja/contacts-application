fe870aae1beea4ba055257cefc92ba9520c89604
Pooja Chopra <chopraapooja@gmail.com>
2017-09-22 21:42:32 +0530

    Refactors add-contact

--- src/contacts_application/contact_directory.clj
+++ src/contacts_application/contact_directory.clj
@@ -19,18 +19,26 @@
           (not-empty (:last-name contact))(update :last-name str/trim) 
           :default                        (update :first-name str/trim)))
 
+(defn- add-contact-to-directory [directory contact]
+  (update directory :contacts #(concat % [contact])))
+
+(defn- index-contact [directory {:keys [first-name last-name]}]
+  (let [contact-index (-> directory :contacts count dec)]
+    (update directory :trie (fn [trie]
+                              (-> trie
+                                  (index-name-in-trie first-name contact-index)
+                                  (index-name-in-trie last-name contact-index))))))
+
 (defn add-contact [directory contact]
   (if (valid-contact? contact)
-    (let [{:keys [first-name last-name] :as contact} (sanatize-contact contact)
-          directory (update directory :contacts #(concat % [contact]))
-          contact-index (-> directory :contacts count dec)] 
-      (update directory :trie (fn [trie]
-                                (-> trie
-                                    (index-name-in-trie first-name contact-index)
-                                    (index-name-in-trie last-name contact-index)))))
+    (let [_contact (sanatize-contact contact)]
+      (-> directory 
+          (add-contact-to-directory _contact)
+          (index-contact _contact)))
     directory))
 
 (defn search-contact [directory search-text]
-  (let [contact-indices (distinct (trie/search-tree (:trie directory) 
-                                            (-> search-text str/trim str/lower-case)))]
+  (let [_search-text (-> search-text str/trim str/lower-case)
+        contact-indices (distinct (trie/search-tree (:trie directory) 
+                                                    _search-text))]
     (utils/pick (:contacts directory) contact-indices)))
